8417a06ec7eb6cd47ff35333e2b244d36c55665a
MaatiTheDev <maatifelix001@gmail.com>
2024-07-14 09:29:26 +0300

    Completed the Scuber distance and fare calculation functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(formData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(object) {
+        const newId = document.createElement('p');
+        newId.textContent = object.id;
+        document.body.appendChild(newId);
+      })
+      .catch(function(error) {
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = error.message;
+        document.body.appendChild(errorMessage);
+      });
+  }
+  
